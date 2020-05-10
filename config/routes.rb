a19f9202bd77dcc490df2925342a8570e1eb39d7
guillermo varela <guillovarela16@gmail.com>
2020-05-10 11:17:35 -0300

    Add articles routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/edit'
-  get 'articles/update'
+
   devise_for :users
   root to: 'pages#home'
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
