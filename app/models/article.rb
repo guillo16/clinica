2d512b5daa3c569a7792af27342d3fd1544c7ad3
guillermo varela <guillovarela16@gmail.com>
2020-05-10 11:14:15 -0300

    Add validations to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, precense: true
+  validates :subtitle, precense: true
+  validates :photo, precense: true
+  validates :content, precense: true
 end
